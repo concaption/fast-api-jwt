f51d75b21c3d80d56a73a731bf2011cfd4f2d974
Usama Navid <concaption@gmail.com>
2023-10-12 13:07:04 +0000

    initial

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#!/usr/bin/env python
+"""
+path: main.py
+
+"""
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def health_check():
+    return JSONResponse(status_code=200, content={"message": "OK"})
